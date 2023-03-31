3212f8aa8dc60b03279c43f0a46a768d77dcb0f5
Prabhakar <prmanav@umich.edu>
2023-03-31 17:17:16 -0400

    Reordered reading data

--- map.py
+++ map.py
@@ -7,7 +7,7 @@ Create a heatmap of the Wi-Fi signal strength in a room from a dataset file
 The heatmap is multi-dimensional; each dimension represents a different Access Point (AP)
 Authors: Arthur L.
 """
-DATASET_FILENAME = r'\Users\nikithaveera\Library\CloudStorage\OneDrive-Umich\Desktop\pythonProject1\HW\dataset.txt'
+DATASET_FILENAME = r'\Users\prmanav\Downloads\dataset.txt'
 #'C:\Users\Nikitha M V\OneDrive - Umich\Desktop\pythonProject1\HW\dataset.txt'
 
 
@@ -17,17 +17,15 @@ def read_dataset_file(filename):
         lines = fp.readlines()
 
     # Parse the dataset file
-    data_points = []
+    data_points = {}
     for line in lines:
         # Skip empty lines
         if line == '\n':
             continue
-
-
+    
         # Parse the line
         line = line.strip()
         line = line.split(',')
-
         if(line[0][0] == '('):
             x, y = float(line[0][1:]), float(line[1][:-1])
             timestamp = line[2]
@@ -40,12 +38,20 @@ def read_dataset_file(filename):
                 signal_strength = float(network[1])
                 ssid = ' '.join(network[2:])
 
-                # Add the data point to the list
-                data_points.append((x, y, signal_strength))
-
+                # Add the data point to the dictionary
+                try:
+                    # if dictionary is empty, create one  
+                    # otherwise append to the dictionary
+                    data_points[bssid]
+                except KeyError:
+                    data_points[bssid]={}
+                try:
+                    data_points[bssid][(x,y)]
+                except KeyError:
+                    data_points[bssid][(x,y)] = []
+                data_points[bssid][(x,y)].append(signal_strength)
+                
     return data_points
-
-
 
 
 
@@ -68,6 +74,11 @@ def create_heatmap(data_points):
 
 if __name__ == "__main__":
     data_points = read_dataset_file(DATASET_FILENAME)
+    #print(data_points[list(data_points.keys())[2]])
     print(data_points)
-   # print(ssid)
-    create_heatmap(data_points)
+    
+    #print(ssid)
+    #print(np.array(data_points)[:60][:,2])
+    #plt.plot(np.array(data_points)[:60][:,2])
+    #plt.show()
+    #create_heatmap(data_points)
